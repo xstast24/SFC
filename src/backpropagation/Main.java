7a3910ac35dbbfb205897fee6e4214e3baccee7a
xstast24 <stastny.F@seznam.cz>
2017-11-26 18:03:12 +0100

    NN initialization

---
+++ src/backpropagation/Main.java
@@ -0,0 +1,11 @@
+package backpropagation;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int inputNeurons = 3;
+	    int hiddenNeurons = 2;
+	    int outputNeurons = 1;
+	    NeuralNetwork network = new NeuralNetwork(inputNeurons, hiddenNeurons, outputNeurons);
+    }
+}
